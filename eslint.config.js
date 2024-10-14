714e9e5c302cf563d6156acf4fa91b372e9de5f6
Artyom Pizin <artempetr2016@gmail.com>
2024-10-14 23:02:10 +0300

    refactor: format files

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     settings: { react: { version: '18.3' } },
-    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -34,4 +38,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,4 @@
 export default {
   experimentalTernaries: true,
   singleQuote: true,
-}
+};
