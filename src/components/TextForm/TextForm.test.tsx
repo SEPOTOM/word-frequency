4cd6b3b8018130836c4a79e1f705244315dc44f2
Artyom Pizin <artempetr2016@gmail.com>
2024-10-18 20:24:10 +0300

    test(TextForm): add a test for restricting chars quantity

--- src/components/TextForm/TextForm.test.tsx
+++ src/components/TextForm/TextForm.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
+import { renderWithUser } from '@/tests';
+
 import TextForm from './TextForm';
 
 describe('TextForm', () => {
@@ -15,5 +17,16 @@ describe('TextForm', () => {
     expect(
       screen.getByRole('button', { name: /translate/i }),
     ).toBeInTheDocument();
+  });
+
+  it('restricts input to a maximum of 2048 characters', async () => {
+    const charsQuantity = 10;
+    const { user } = renderWithUser(<TextForm maxChars={charsQuantity} />);
+    const inputText = 'a'.repeat(charsQuantity);
+    const textarea = screen.getByRole('textbox');
+
+    await user.type(textarea, inputText + 'b');
+
+    expect(textarea).toHaveDisplayValue(inputText);
   });
 });
