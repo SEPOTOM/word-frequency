f7cbe3f97d98930b952044b39725ea1ffd2b99e2
Artyom Pizin <artempetr2016@gmail.com>
2024-10-20 18:19:13 +0300

    test(FrequencyTable): add a test to display the frequency data correctly

--- src/components/FrequencyTable/FrequencyTable.test.tsx
+++ src/components/FrequencyTable/FrequencyTable.test.tsx
@@ -1,19 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
+
+import { FrequencyDatum } from '@/types';
 
 import FrequencyTable from './FrequencyTable';
 
 describe('FrequencyTable', () => {
   it('should display a table', () => {
-    render(<FrequencyTable />);
+    render(<FrequencyTable frequencyData={[]} />);
 
     expect(screen.getByRole('table')).toBeInTheDocument();
   });
 
   it('should display a caption', () => {
-    render(<FrequencyTable />);
+    render(<FrequencyTable frequencyData={[]} />);
 
     expect(
       screen.getByRole('caption', { name: /frequency/i }),
     ).toBeInTheDocument();
+  });
+
+  it('should properly display the received frequency data', () => {
+    const fakeFrequencyData: FrequencyDatum[] = [
+      {
+        entity: 'word',
+        repetitionsAmount: 23,
+      },
+      {
+        entity: 'Title',
+        repetitionsAmount: 57,
+      },
+    ];
+    render(<FrequencyTable frequencyData={fakeFrequencyData} />);
+
+    const rows = screen.getAllByRole('row');
+    expect(rows.length).toBe(2);
+
+    const firstRowCells = within(rows[0]).getAllByRole('cell');
+    expect(firstRowCells.length).toBe(2);
+    expect(firstRowCells[0]).toHaveTextContent('word');
+    expect(firstRowCells[1]).toHaveTextContent('23');
+
+    const secondRowCells = within(rows[1]).getAllByRole('cell');
+    expect(secondRowCells.length).toBe(2);
+    expect(secondRowCells[0]).toHaveTextContent('Title');
+    expect(secondRowCells[1]).toHaveTextContent('57');
   });
 });
