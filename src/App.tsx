0ceb3f2a77ce1345266b5df1e813f2fec55b51f1
Artyom Pizin <artempetr2016@gmail.com>
2024-10-20 19:00:07 +0300

    feat(App): retrieve text from TextForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+
 import { FrequencyTable, TextForm } from '@/components';
 
-const App = () => (
-  <>
-    <TextForm maxChars={2048} />
-    <FrequencyTable frequencyData={[]} />
-  </>
-);
+const App = () => {
+  const [_text, setText] = useState('');
+
+  const handleSubmit = (newText: string) => {
+    setText(newText);
+  };
+
+  return (
+    <>
+      <TextForm maxChars={2048} onSubmit={handleSubmit} />
+      <FrequencyTable frequencyData={[]} />
+    </>
+  );
+};
 
 export default App;
