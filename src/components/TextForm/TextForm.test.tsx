881af1097bb89ffde5eec367719ae70899bfcdfa
Artyom Pizin <artempetr2016@gmail.com>
2024-10-21 18:16:47 +0300

    test(TextForm): add missing props

--- src/components/TextForm/TextForm.test.tsx
+++ src/components/TextForm/TextForm.test.tsx
@@ -6,13 +6,13 @@ import TextForm from './TextForm';
 
 describe('TextForm', () => {
   it('should display a text input field', () => {
-    render(<TextForm maxChars={10} />);
+    render(<TextForm maxChars={10} onSubmit={() => null} />);
 
     expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 
   it('should display a translate button', () => {
-    render(<TextForm maxChars={10} />);
+    render(<TextForm maxChars={10} onSubmit={() => null} />);
 
     expect(
       screen.getByRole('button', { name: /translate/i }),
@@ -21,7 +21,9 @@ describe('TextForm', () => {
 
   it('restricts input to a maximum of 2048 characters', async () => {
     const charsQuantity = 10;
-    const { user } = renderWithUser(<TextForm maxChars={charsQuantity} />);
+    const { user } = renderWithUser(
+      <TextForm maxChars={charsQuantity} onSubmit={() => null} />,
+    );
     const inputText = 'a'.repeat(charsQuantity);
     const textarea = screen.getByRole('textbox');
 
