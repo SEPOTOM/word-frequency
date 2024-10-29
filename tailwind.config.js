322ca06a0f19e1bbca8ee3d8a18fe1e4e26228c3
Artyom Pizin <artempetr2016@gmail.com>
2024-10-29 23:57:22 +0300

    config(tailwind): extend theme with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#64113F',
+        secondary: '#F5F7DC',
+      },
+    },
   },
   plugins: [],
 };
