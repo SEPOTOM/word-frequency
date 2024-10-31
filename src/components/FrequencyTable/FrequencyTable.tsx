2417f602cd57cc68c6e62fac4284e1658701ad80
Artyom Pizin <artempetr2016@gmail.com>
2024-10-31 23:18:28 +0300

    feat(FrequencyTable): add styles for mobiles

--- src/components/FrequencyTable/FrequencyTable.tsx
+++ src/components/FrequencyTable/FrequencyTable.tsx
@@ -1,17 +1,31 @@
 import { FC } from 'react';
 
+import clsx from 'clsx';
+
 import { FrequencyTableProps } from './types';
 
 const FrequencyTable: FC<FrequencyTableProps> = ({ frequencyData }) => (
-  <table>
-    <caption>Frequency Table</caption>
+  <table className="container border-b-2 border-main bg-inherit">
+    <caption className="bg-main py-2 text-xl text-secondary">
+      Frequency Table
+    </caption>
     <tbody>
-      {frequencyData.map(({ entity, repetitionsAmount }) => (
-        <tr key={`${entity}${repetitionsAmount}`}>
-          <td>{entity}</td>
-          <td>{repetitionsAmount}</td>
-        </tr>
-      ))}
+      {frequencyData.map(({ entity, repetitionsAmount }, index) => {
+        const isEven = index % 2 === 0;
+
+        return (
+          <tr
+            key={`${entity}${repetitionsAmount}`}
+            className={clsx(
+              'text-lg',
+              isEven ? 'bg-inherit text-main' : 'bg-main text-secondary',
+            )}
+          >
+            <td className="px-2 py-1">{entity}</td>
+            <td className="px-2 py-1 font-bold">{repetitionsAmount}</td>
+          </tr>
+        );
+      })}
     </tbody>
   </table>
 );
