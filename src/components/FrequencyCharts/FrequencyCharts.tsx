80ccee6840179e93fa6758d88b90eb1e7ccea66d
Artyom Pizin <artempetr2016@gmail.com>
2024-11-02 00:19:25 +0300

    feat(FrequencyCharts): make component fully adaptive and show pie chart for mobiles

--- src/components/FrequencyCharts/FrequencyCharts.tsx
+++ src/components/FrequencyCharts/FrequencyCharts.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
 
+import { useWindowSize } from '@uidotdev/usehooks';
 import {
   Bar,
   BarChart,
   CartesianGrid,
   Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -17,21 +21,43 @@ import { FrequencyChartsProps } from './types';
 const FrequencyCharts: FC<FrequencyChartsProps> = ({ frequencyData }) => {
   const tailwindConfig = getTailwindConfig();
 
+  const windowSize = useWindowSize();
+  const windowWidth = windowSize.width ?? 304;
+
+  const isMobile = windowWidth < parseInt(tailwindConfig.theme.screens.md);
+  const containerHeight = isMobile ? windowWidth : 400;
+
   return (
-    <div className="container px-2">
-      <BarChart width={304} height={400} data={frequencyData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="entity" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar
-          dataKey="repetitionsAmount"
-          name="Repetitions"
-          fill={tailwindConfig.theme.colors.main}
-        />
-      </BarChart>
-    </div>
+    <ResponsiveContainer
+      width="100%"
+      height={containerHeight}
+      className="mx-auto w-full max-w-3xl px-2"
+    >
+      {isMobile ?
+        <PieChart className="w-full">
+          <Tooltip />
+          <Pie
+            data={frequencyData}
+            dataKey="repetitionsAmount"
+            nameKey="entity"
+            fill={tailwindConfig.theme.colors.main}
+            label
+          />
+        </PieChart>
+      : <BarChart data={frequencyData} className="mx-auto">
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="entity" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="repetitionsAmount"
+            name="Repetitions"
+            fill={tailwindConfig.theme.colors.main}
+          />
+        </BarChart>
+      }
+    </ResponsiveContainer>
   );
 };
 
