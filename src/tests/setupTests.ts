0a8156ca433995bcdef4aaa62b091fdab8bd403f
Artyom Pizin <artempetr2016@gmail.com>
2024-11-02 00:59:45 +0300

    test: mock ResizeObserver in the setup file

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -4,3 +4,9 @@ import { cleanup } from '@testing-library/react';
 afterEach(() => {
   cleanup();
 });
+
+window.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
