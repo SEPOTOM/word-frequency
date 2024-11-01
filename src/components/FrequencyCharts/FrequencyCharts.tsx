a1a1718f622106f5cee222602a5f8e67ec207a5e
Artyom Pizin <artempetr2016@gmail.com>
2024-11-02 01:07:45 +0300

    feat(FrequencyCharts): display nothing if the frequencyData is empty

--- src/components/FrequencyCharts/FrequencyCharts.tsx
+++ src/components/FrequencyCharts/FrequencyCharts.tsx
@@ -27,38 +27,38 @@ const FrequencyCharts: FC<FrequencyChartsProps> = ({ frequencyData }) => {
   const isMobile = windowWidth < parseInt(tailwindConfig.theme.screens.md);
   const containerHeight = isMobile ? windowWidth : 400;
 
-  return (
-    <ResponsiveContainer
-      width="100%"
-      height={containerHeight}
-      className="mx-auto w-full max-w-3xl px-2"
-    >
-      {isMobile ?
-        <PieChart className="w-full">
-          <Tooltip />
-          <Pie
-            data={frequencyData}
-            dataKey="repetitionsAmount"
-            nameKey="entity"
-            fill={tailwindConfig.theme.colors.main}
-            label
-          />
-        </PieChart>
-      : <BarChart data={frequencyData} className="mx-auto">
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="entity" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar
-            dataKey="repetitionsAmount"
-            name="Repetitions"
-            fill={tailwindConfig.theme.colors.main}
-          />
-        </BarChart>
-      }
-    </ResponsiveContainer>
-  );
+  return frequencyData.length > 0 ?
+      <ResponsiveContainer
+        width="100%"
+        height={containerHeight}
+        className="mx-auto w-full max-w-3xl px-2"
+      >
+        {isMobile ?
+          <PieChart className="w-full">
+            <Tooltip />
+            <Pie
+              data={frequencyData}
+              dataKey="repetitionsAmount"
+              nameKey="entity"
+              fill={tailwindConfig.theme.colors.main}
+              label
+            />
+          </PieChart>
+        : <BarChart data={frequencyData} className="mx-auto">
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="entity" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar
+              dataKey="repetitionsAmount"
+              name="Repetitions"
+              fill={tailwindConfig.theme.colors.main}
+            />
+          </BarChart>
+        }
+      </ResponsiveContainer>
+    : null;
 };
 
 export default FrequencyCharts;
