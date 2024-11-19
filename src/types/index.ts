4e57ff52315eed67d246571088ae07067f02267c
Artyom Pizin <artempetr2016@gmail.com>
2024-11-19 19:35:40 +0300

    feat: add ParsingOptions interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 import { FrequencyDatum } from '@/types/frequency';
+import { ParsingOptions } from '@/types/options';
 
-export type { FrequencyDatum };
+export type { FrequencyDatum, ParsingOptions };
---
+++ src/types/options.ts
@@ -0,0 +1,3 @@
+export interface ParsingOptions {
+  caseSensitive: boolean;
+}
