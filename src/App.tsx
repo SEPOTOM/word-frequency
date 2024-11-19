ee29265366c36afa79281487bc68625bea0e3af6
Artyom Pizin <artempetr2016@gmail.com>
2024-11-19 20:03:03 +0300

    feat(App): display ParsingOptionsPanel with caseSensitive set to true by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 
-import { FrequencyCharts, FrequencyTable, TextForm } from '@/components';
+import {
+  FrequencyCharts,
+  FrequencyTable,
+  ParsingOptionsPanel,
+  TextForm,
+} from '@/components';
 import { useWordsFrequency } from '@/hooks';
+import { ParsingOptions } from '@/types';
 
 const App = () => {
   const [text, setText] = useState('');
+  const [parsingOptions, setParsingOptions] = useState<ParsingOptions>({
+    caseSensitive: true,
+  });
   const wordsFrequency = useWordsFrequency(text);
 
   const handleSubmit = (newText: string) => {
@@ -13,6 +22,10 @@ const App = () => {
 
   return (
     <div className="flex flex-col gap-12 bg-inherit py-5">
+      <ParsingOptionsPanel
+        options={parsingOptions}
+        onOptionsChange={(newOptions) => setParsingOptions(newOptions)}
+      />
       <TextForm maxChars={2048} onSubmit={handleSubmit} />
       <FrequencyTable frequencyData={wordsFrequency} />
       <FrequencyCharts frequencyData={wordsFrequency} />
