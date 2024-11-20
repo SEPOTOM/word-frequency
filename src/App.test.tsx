bec50818d3f57d2cf5d5f559de54dea075f2a01e
Artyom Pizin <artempetr2016@gmail.com>
2024-11-20 15:02:47 +0300

    test(App): add test for calculating amount of each letter when lettersOnly is checked

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -79,5 +79,22 @@ describe('App', () => {
 
       expect(screen.getAllByRole('row').length).toBe(1);
     });
+
+    it('should calculate the number of each letter when the lettersOnly is checked', async () => {
+      const optionName = 'lettersOnly' satisfies keyof ParsingOptions;
+      const { user } = renderWithUser(<App />);
+
+      await user.click(screen.getByRole('checkbox', { name: optionName }));
+      await user.type(screen.getByRole('textbox'), 'test message');
+      await user.click(screen.getByRole('button', { name: /translate/i }));
+
+      expect(screen.getAllByRole('row').length).toBe(6);
+      expect(screen.getByRole('row', { name: /t 2/ })).toBeInTheDocument();
+      expect(screen.getByRole('row', { name: /e 3/ })).toBeInTheDocument();
+      expect(screen.getByRole('row', { name: /s 3/ })).toBeInTheDocument();
+      expect(screen.getByRole('row', { name: /m 1/ })).toBeInTheDocument();
+      expect(screen.getByRole('row', { name: /a 1/ })).toBeInTheDocument();
+      expect(screen.getByRole('row', { name: /g 1/ })).toBeInTheDocument();
+    });
   });
 });
