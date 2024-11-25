6545f3eaec1e70756da12487032dfc83cc918c0a
Artyom Pizin <artempetr2016@gmail.com>
2024-11-25 19:52:04 +0300

    feat(shortenResult): decrease the result's length to the needed one

--- src/utils/frequency/index.ts
+++ src/utils/frequency/index.ts
@@ -3,8 +3,13 @@ import { FrequencyDatum } from '@/types';
 export const shortenResult = (
   fullResult: FrequencyDatum[],
   neededLength: number,
-) => {
+): FrequencyDatum[] => {
   if (fullResult.length <= neededLength) {
     return fullResult;
   }
+
+  const shortenedResult = [...fullResult];
+  shortenedResult.length = neededLength;
+
+  return shortenedResult;
 };
