08859bfb087e8b28101487840d74a703a06efbd8
Artyom Pizin <artempetr2016@gmail.com>
2024-11-25 19:44:43 +0300

    test(shortenResult): add test for returning result if its length is equal to needed one

--- src/utils/frequency/frequency.test.ts
+++ src/utils/frequency/frequency.test.ts
@@ -13,4 +13,15 @@ describe('shortenResult', () => {
 
     expect(result).toBe(input[0]);
   });
+
+  it('should return the same array if its length is equal to the needed one', () => {
+    const input: [FrequencyDatum[], number] = [
+      [{ entity: 'Test', repetitionsAmount: 2 }],
+      1,
+    ];
+
+    const result = shortenResult(...input);
+
+    expect(result).toBe(input[0]);
+  });
 });
