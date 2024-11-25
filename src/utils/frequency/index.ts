10b1792d0005d858e182e40c742edca3b261c847
Artyom Pizin <artempetr2016@gmail.com>
2024-11-25 19:42:39 +0300

    feat(shortenResult): return the result if it is shorter than needed length

--- src/utils/frequency/index.ts
+++ src/utils/frequency/index.ts
@@ -1,1 +1,10 @@
-export const shortenResult = () => null;
+import { FrequencyDatum } from '@/types';
+
+export const shortenResult = (
+  fullResult: FrequencyDatum[],
+  neededLength: number,
+) => {
+  if (fullResult.length < neededLength) {
+    return fullResult;
+  }
+};
