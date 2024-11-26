a8d927402f0e70ba15fc0f95cfd74f5b950e464b
Artyom Pizin <artempetr2016@gmail.com>
2024-11-26 18:06:58 +0300

    feat(ParsingOptionsPanel): add heading

--- src/components/ParsingOptionsPanel/ParsingOptionsPanel.tsx
+++ src/components/ParsingOptionsPanel/ParsingOptionsPanel.tsx
@@ -19,23 +19,28 @@ const ParsingOptionsPanel: FC<ParsingOptionsPanelProps> = ({
   };
 
   return (
-    <ul className="mx-auto flex w-full max-w-3xl flex-col gap-y-3 px-2 text-lg font-bold text-main">
-      {Object.keys(options).map((optionKey) => (
-        <li
-          key={optionKey}
-          className="last:border-b-2 last:border-main last:pb-2"
-        >
-          <label className="flex items-center justify-between gap-x-3">
-            {optionKey}
-            <Checkbox
-              name={optionKey}
-              checked={options[optionKey as keyof ParsingOptions]}
-              onChange={onChange}
-            />
-          </label>
-        </li>
-      ))}
-    </ul>
+    <div className="mx-auto flex w-full max-w-3xl flex-col gap-y-3 px-2">
+      <h3 className="bg-main py-1 text-center text-2xl font-bold text-secondary">
+        Options
+      </h3>
+      <ul className="flex flex-col gap-y-3 text-lg font-bold text-main">
+        {Object.keys(options).map((optionKey) => (
+          <li
+            key={optionKey}
+            className="last:border-b-2 last:border-main last:pb-2"
+          >
+            <label className="flex items-center justify-between gap-x-3">
+              {optionKey}
+              <Checkbox
+                name={optionKey}
+                checked={options[optionKey as keyof ParsingOptions]}
+                onChange={onChange}
+              />
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
